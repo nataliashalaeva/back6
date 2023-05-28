bc2023718eebd75cbfd42b6e1bee4ac0b5b38da4
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2023-05-28 21:38:38 +0300

    Update login.php

--- login.php
+++ login.php
@@ -27,6 +27,7 @@ if (!empty($_SESSION['login'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
+
 <form action="login.php" method="post">
   <input name="login" />
   <input name="password" type ="password"/>
